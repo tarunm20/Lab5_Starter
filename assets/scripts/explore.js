b87a7c7669b2603ed051dc8a27feef1c6457de09
tarunm20 <tarun.murugan24@gmail.com>
2023-05-07 14:59:31 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let voices = [];
+  const synth = window.speechSynthesis;
+  const select = document.getElementById('voice-select');
+  const getVoices = () => {
+    voices = synth.getVoices();
+    voices.forEach(voice => {
+      var option = document.createElement('option');
+      option.value = voice.name;
+      option.textContent = voice.name + ' (' + voice.lang + ')';
+      option.setAttribute('data-name', voice.name);
+      select.appendChild(option);
+    });
+  }
+  if(synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+  }
+
+  const speak = () => {
+    const text = document.getElementById('text-to-speak').value;
+    console.log(text);
+    var speakText = new SpeechSynthesisUtterance(text);
+    voices.forEach(voice => {
+      if(voice.name == select.value) {
+        speakText.voice = voice;
+      }
+    });
+    const face = document.getElementsByTagName('img')[0];
+    speakText.onstart = e => {
+      face.src = '../assets/images/smiling-open.png';
+    }
+    speakText.onend = e => {
+      face.src = '../assets/images/smiling.png';
+    }
+    synth.speak(speakText);
+  }
+  const button = document.getElementsByTagName('button')[0];
+  button.addEventListener('click', () => {
+    speak();
+  })
 }
